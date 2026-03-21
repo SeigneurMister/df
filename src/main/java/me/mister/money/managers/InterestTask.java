4189642ab9167c405e25977c6efd7523f6ae88b7
D_Mathéo <mister.matheo2005@gmail.com>
2026-03-21 00:57:08 +0000

    dfg

--- src/main/java/me/mister/money/managers/InterestTask.java
+++ src/main/java/me/mister/money/managers/InterestTask.java
@@ -12,16 +12,17 @@ public class InterestTask {
     private final MoneyPlugin plugin;
     private BukkitTask task;
 
-    // 2 jours en ms
-    private static final long INTERVAL_MS = 2L * 24L * 60L * 60L * 1000L;
-    private static final double RATE = 0.0175;
+    // 1 minute en ms
+    private static final long INTERVAL_MS = 60L * 1000L;
+    private static final double RATE = 0.0175; // 1.75%
 
     public InterestTask(MoneyPlugin plugin) {
         this.plugin = plugin;
     }
 
     public void start() {
-        // Vérification toutes les 5 minutes
+
+        // Vérification toutes les minutes
         this.task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
 
             long now = System.currentTimeMillis();
@@ -43,7 +44,7 @@ public class InterestTask {
                 }
             }
 
-        }, 20L, 20L * 60L * 5L);
+        }, 20L, 20L * 60L);
     }
 
     public void stop() {
